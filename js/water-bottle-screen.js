4066a58280cd36772b86fb8517164cd6b47da002
ClayAucoin <clay0aucoin@gmail.com>
2025-08-26 20:37:39 -0500

    added disable button to water-bottle-screen page

--- js/water-bottle-screen.js
+++ js/water-bottle-screen.js
@@ -2,19 +2,19 @@
 var getLang = new URLSearchParams(window.location.search);
 var language = getLang.get('lang');
 
-if(language == "es"){
+if (language == "es") {
     document.getElementById("title").innerHTML = "Consejos de Conservación<br>del Agua";
-`    document.getElementById("subTitle").innerHTML = "En lugar de comprar botellas de agua desechables, usa una botella reutilizable. Las botellas de un solo uso llenan los vertederos y desperdician agua valiosa.";
+    document.getElementById("subTitle").innerHTML = "En lugar de comprar botellas de agua desechables, usa una botella reutilizable. Las botellas de un solo uso llenan los vertederos y desperdician agua valiosa.";
     document.getElementById("instruction").innerHTML = "Elige la botella de agua correcta.<br>Luego, haz clic en el grifo para llenarla.";
     document.getElementById("nextButton").setAttribute("href", "shower-screen.html?lang=es");
-`}
-
-
+}
 
 document.getElementById("plastic-bottle").addEventListener("click", () => {
     document.getElementById("ban").style.opacity = "1";
     document.getElementById("glass-bottle").style.opacity = "1";
     document.getElementById("bottle-under-faucet").style.opacity = "0";
+    document.getElementById('nextButton').classList.add('disabled');
+
 
     // playMySound("audio/pacman-death.mp3", false, 0.4);
 });
@@ -23,6 +23,7 @@ document.getElementById("glass-bottle").addEventListener("click", () => {
     document.getElementById("ban").style.opacity = "0";
     document.getElementById("glass-bottle").style.opacity = "0";
     document.getElementById("bottle-under-faucet").style.opacity = "1";
+    document.getElementById('nextButton').classList.remove('disabled');
 
     // playMySound("audio/success.mp3", false, 0.4);
 });
