7be086d8dee2ae7231763402523493ad8633debe
ClayAucoin <clay0aucoin@gmail.com>
2025-08-28 09:42:22 -0500

    uncommented sound

--- js/shower-screen.js
+++ js/shower-screen.js
@@ -13,7 +13,7 @@ document.getElementById("timer").addEventListener("click", () => {
     document.getElementById("timer").style.opacity = "0";
     document.getElementById("times-up").style.opacity = "1";
 
-    // playMySound("audio/ding-cartoon.mp3", false, 0.4);
+    playMySound("audio/ding-cartoon.mp3", false, 0.4);
 });
 
 document.getElementById("shower-duck").addEventListener("click", () => {
@@ -23,5 +23,5 @@ document.getElementById("shower-duck").addEventListener("click", () => {
 });
 
 document.getElementById("rubber-duck").addEventListener("click", () => {
-    // playMySound("audio/water-bubbles.mp3", false, 0.4);
+    playMySound("audio/water-bubbles.mp3", false, 0.4);
 });
